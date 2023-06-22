3be002f7b1fe12cce782ab7e68e7c4cb46f94ae7
Heyz <41371869+popka12@users.noreply.github.com>
2023-06-22 15:51:47 +0300

    g
    gf

---
+++ main.js
@@ -0,0 +1,19 @@
+function Question_1(el){
+    alert("Повідомлення 1");
+}
+
+function Question_2(el){
+    alert("Повідомлення 2");
+}
+
+function Question_3(el){
+    alert("Повідомлення 3");
+}
+
+function Question_4(el){
+    alert("Повідомлення 4");
+}
+
+function Question_5(el){
+    alert("Повідомлення 5");
+}
